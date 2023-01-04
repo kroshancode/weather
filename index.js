e8417cb2838fc3ad0d4346a83a47e0b77915e526
kroshancode <87307413+kroshancode@users.noreply.github.com>
2023-01-04 14:41:40 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const options = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': 'b435db8b02mshf1d7905ef8bcbcbp172da8jsna3209c3c0265',
+        'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+    }
+};
+
+
+const getWeather = (city) => {
+
+    cityName.innerHTML = city
+    fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' + city, options)
+        .then(response => response.json())
+        .then((response) => {
+
+
+            console.log(response)
+
+
+            cloud_pct.innerHTML = response.cloud_pct
+            temp.innerHTML = response.temp
+            temp2.innerHTML = response.temp
+            feels_like.innerHTML = response.feels_like
+            humidity2.innerHTML = response.humidity
+            humidity.innerHTML = response.humidity
+            min_temp.innerHTML = response.min_temp
+            max_temp.innerHTML = response.max_temp
+            wind_speed.innerHTML = response.wind_speed
+            wind_degrees.innerHTML = response.wind_degrees
+            sunrise.innerHTML = response.sunrise
+            sunset.innerHTML = response.sunset
+
+        })
+        .catch(err => console.error(err));
+}
+
+submit.addEventListener("click", (e) => {
+    e.preventDefault
+    getWeather(city.value)
+});
+
+getWeather("Delhi");
